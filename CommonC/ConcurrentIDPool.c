745164ce19324d5279568683a1e4988bc5eeaa21
Stefan Johnson <sj@scrimpycat.io>
2019-01-01 22:35:53 +1100

    Implement basic id pool

--- CommonC/ConcurrentIDPool.c
+++ CommonC/ConcurrentIDPool.c
@@ -24,3 +24,68 @@
  */
 
 #include "ConcurrentIDPool.h"
+#include "MemoryAllocation.h"
+#include "Assertion.h"
+#include <stdatomic.h>
+
+typedef struct CCConcurrentIDPoolInfo {
+    size_t size;
+    _Atomic(uint8_t) pool[];
+} CCConcurrentIDPoolInfo;
+
+CCConcurrentIDPool CCConcurrentIDPoolCreate(CCAllocatorType Allocator, size_t PoolSize)
+{
+    CCAssertLog(PoolSize >= 1, "PoolSize must be at least 1");
+    
+    CCConcurrentIDPool IDPool = CCMalloc(Allocator, sizeof(CCConcurrentIDPoolInfo) + (sizeof(typeof(IDPool->pool[0])) * PoolSize), NULL, CC_DEFAULT_ERROR_CALLBACK);
+    if (IDPool)
+    {
+        IDPool->size = PoolSize;
+        for (size_t Loop = 0; Loop < PoolSize; Loop++) atomic_init(&IDPool->pool[Loop], 0);
+    }
+    
+    return IDPool;
+}
+
+void CCConcurrentIDPoolDestroy(CCConcurrentIDPool IDPool)
+{
+    CCAssertLog(IDPool, "IDPool must not be null");
+    CCFree(IDPool);
+}
+
+size_t CCConcurrentIDPoolAssign(CCConcurrentIDPool IDPool)
+{
+    size_t ID;
+    while (!CCConcurrentIDPoolTryAssign(IDPool, &ID)) CC_SPIN_WAIT();
+    
+    return ID;
+}
+
+_Bool CCConcurrentIDPoolTryAssign(CCConcurrentIDPool IDPool, size_t *ID)
+{
+    for (size_t Loop = 0, Count = IDPool->size; Loop < Count; Loop += (sizeof(uint64_t) / sizeof(typeof(IDPool->pool[0]))))
+    {
+        uint64_t Sample = atomic_load_explicit((_Atomic(uint64_t)*)&IDPool->pool[Loop], memory_order_relaxed);
+        if (Sample != UINT64_MAX)
+        {
+            for (size_t Loop2 = Loop, SampleCount = Loop + (sizeof(uint64_t) / sizeof(typeof(IDPool->pool[0]))); Loop2 < SampleCount; Loop2++)
+            {
+                if (atomic_exchange_explicit(&IDPool->pool[Loop2], UINT8_MAX, memory_order_relaxed) == 0)
+                {
+                    *ID = Loop2;
+                    atomic_thread_fence(memory_order_acquire);
+                    return TRUE;
+                }
+            }
+        }
+    }
+    
+    return FALSE;
+}
+
+void CCConcurrentIDPoolRecycle(CCConcurrentIDPool IDPool, size_t ID)
+{
+    CCAssertLog(ID < IDPool->size, "ID must have been assigned from this pool");
+    
+    atomic_store_explicit(&IDPool->pool[ID], 0, memory_order_release);
+}
