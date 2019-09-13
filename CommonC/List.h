97a7b3f7daf9b69e9d12a7e85944a850bec79514
Stefan Johnson <sj@scrimpycat.io>
2019-09-13 17:40:28 +1000

    Add definition for list

---
+++ CommonC/List.h
@@ -0,0 +1,165 @@
+/*
+ *  Copyright (c) 2015, Stefan Johnson
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this list
+ *     of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this
+ *     list of conditions and the following disclaimer in the documentation and/or other
+ *     materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*!
+ * @header CCList
+ * CCList implements a linked list of arrays.
+ *
+ * The intended use case is when you desire to have spatial locality of elements,
+ * but may need the list to grow indefinitely (and wish to avoid more expensive
+ * copies as the list gets bigger). It provides an interface comparable to @b CCArray.
+ */
+
+#ifndef CommonC_List_h
+#define CommonC_List_h
+
+#include <CommonC/Base.h>
+#include <CommonC/Allocator.h>
+#include <CommonC/Assertion.h>
+#include <CommonC/Array.h>
+#include <CommonC/LinkedList.h>
+
+typedef struct CCListInfo {
+    size_t count;
+    size_t max;
+    CCLinkedList list;
+    CCAllocator allocator;
+} CCListInfo;
+
+/*!
+ * @brief The list.
+ * @description Allows @b CCRetain.
+ */
+typedef struct CCListInfo *CCList;
+
+
+#pragma mark - Creation/Destruction
+/*!
+ * @brief Create a list.
+ * @param Allocator The allocator to be used for the allocation.
+ * @param ElementSize The size of the data elements.
+ * @param ChunkSize The number of elements to fit with each allocation. Must be at least 1.
+ * @param PageMax The maximum size of a single page. Must be at least 1.
+ * @return An empty list, or NULL on failure. Must be destroyed to free the memory.
+ */
+CC_NEW CCList CCListCreate(CCAllocatorType Allocator, size_t ElementSize, size_t ChunkSize, size_t PageMax);
+
+/*!
+ * @brief Destroy an list.
+ * @param List The list to be destroyed.
+ */
+void CCListDestroy(CCList CC_DESTROY(List));
+
+
+#pragma mark - Insertions/Deletions
+/*!
+ * @brief Appends the element to the end of the list.
+ * @description Increases the list's count by 1.
+ * @warning The size of element must be the same size as specified in the list creation.
+ * @param List The list to append the element to.
+ * @param Element The pointer to the element to be copied to the end of the list. If NULL it
+ *        will create an uninitialized element.
+ *
+ * @return The index the element was added or SIZE_MAX on failure.
+ */
+size_t CCListAppendElement(CCList List, const void *Element);
+
+/*!
+ * @brief Replace element at index with new element.
+ * @warning The size of element must be the same size as specified in the list creation. And the
+ *          Index must not be out of bounds.
+ *
+ * @param List The list to replace an element of.
+ * @param Index The position of the element to be replaced.
+ * @param Element The replacement element. If NULL the operation does nothing.
+ */
+void CCListReplaceElementAtIndex(CCList List, size_t Index, const void *Element);
+
+/*!
+ * @brief Insert an element at a given index into the list.
+ * @description Increases the list's count by 1.
+ * @performance The further away from the end of the list the index is, the slower it is. It has
+ *              a worst case of O(n+p) (where p is the number of pages).
+ *
+ * @warning The size of element must be the same size as specified in the list creation. And the
+ *          Index must not be out of bounds
+ *
+ * @param List The list to insert the element into.
+ * @param Index The position in the list for the element to be inserted.
+ * @param Element The pointer to the element to be copied to the give position in the list. If NULL
+ *        it will create an uninitialized element.
+ *
+ * @return The index the element was added or SIZE_MAX on failure.
+ */
+size_t CCListInsertElementAtIndex(CCList List, size_t Index, const void *Element);
+
+/*!
+ * @brief Removes an element at a given index from the list.
+ * @description Decreases the list's count by 1.
+ * @performance The further away from the end of the list the index is, the slower it is. It has
+ *              a worst case of O(n+p) (where p is the number of pages).
+ *
+ * @warning Index must not be out of bounds
+ * @param List The list to remove an element from.
+ * @param Index The position in the list for the element to be removed from.
+ */
+void CCListRemoveElementAtIndex(CCList List, size_t Index);
+
+/*!
+ * @brief Removes all elements from the list.
+ * @description Sets the list's count to 0.
+ * @param List The list to remove all elements from.
+ */
+void CCListRemoveAllElements(CCList List);
+
+
+#pragma mark - Query Info
+/*!
+ * @brief Get the current number of elements in the list.
+ * @param List The list to get the count of.
+ * @return The number of elements.
+ */
+static inline size_t CCListGetCount(CCList List);
+
+/*!
+ * @brief Get the element size of the list.
+ * @param List The list to get the element size of.
+ * @return The size of elements.
+ */
+static inline size_t CCListGetElementSize(CCList List);
+
+/*!
+ * @brief Get the element at index.
+ * @warning Index must not be out of bounds.
+ * @param List The list to get the element of.
+ * @param Index The index of the element.
+ * @return The pointer to the element.
+ */
+static inline void *CCListGetElementAtIndex(CCList List, size_t Index);
+
+
+#pragma mark -
+
+#endif
