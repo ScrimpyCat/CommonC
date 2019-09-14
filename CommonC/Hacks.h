195e91cb12e0062a9b8e7e8794df73dfe4d62d94
Stefan Johnson <sj@scrimpycat.io>
2019-09-14 12:19:24 +1000

    Add convenience macro to get argument count

--- CommonC/Hacks.h
+++ CommonC/Hacks.h
@@ -28,6 +28,7 @@
  consistency depending on what functionality is available.
  
  CC_SILENCE_UNUSED_WARNING: Silences the unused value/variable warning where it would otherwise not be appropriate or sometimes even able to use a compiler extension.
+ CC_VA_ARG_COUNT: Get the argument count for __VA_ARGS__.
  */
 
 #ifndef CommonC_Hacks_h
@@ -63,5 +64,7 @@ CC_GENERIC_ATTRIBUTE_RETURN_VALUE_FUNCTION_(long_double, long double), \
 default: CC_NAME_RETURN_VALUE_FUNCTION_(void_ptr) \
 ))(val)
 
+#define CC_VA_ARG_COUNT(...) CC_VA_ARG_COUNT_(0, ## __VA_ARGS__, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define CC_VA_ARG_COUNT_(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, count, ...) count
 
 #endif
