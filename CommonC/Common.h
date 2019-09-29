89b8f4af43d58f1d31728d329a30d7c5c9e7d79f
Stefan Johnson <sj@scrimpycat.io>
2019-09-30 05:39:53 +1000

    Expose enumerable

--- CommonC/Common.h
+++ CommonC/Common.h
@@ -66,6 +66,7 @@
 
 #include <CommonC/Comparator.h>
 #include <CommonC/Enumerator.h>
+#include <CommonC/Enumerable.h>
 #include <CommonC/LinkedList.h>
 #include <CommonC/Array.h>
 #include <CommonC/List.h>
