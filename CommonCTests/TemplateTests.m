8f85fe534e3c5e111a0b01c82fe52f4b49cd5dc4
Stefan Johnson <sj@scrimpycat.io>
2020-10-02 02:23:55 +1000

    Test mangled type signatures

--- CommonCTests/TemplateTests.m
+++ CommonCTests/TemplateTests.m
@@ -34,11 +34,12 @@
 
 @implementation TemplateTests
 
+#define STR(x) STR_(x)
+#define STR_(x) [NSString stringWithUTF8String: #x]
+
 -(void) testExtractingTypes
 {
 #define EXTRACT(...) STR(CC_TYPE(__VA_ARGS__))
-#define STR(x) STR_(x)
-#define STR_(x) [NSString stringWithUTF8String: #x]
     
     XCTAssertEqualObjects(EXTRACT(int), @"int", @"should have correct value");
     XCTAssertEqualObjects(EXTRACT(int var), @"int", @"should have correct value");
@@ -56,6 +57,23 @@
     XCTAssertEqualObjects(EXTRACT(TYPE(foo) var), @"TYPE(foo)", @"should have correct value");
     XCTAssertEqualObjects(EXTRACT(PTYPE(int) *), @"PTYPE(int)", @"should have correct value");
     XCTAssertEqualObjects(EXTRACT(FPTYPE(int(*)(int, int))), @"FPTYPE(int(*)(int, int))", @"should have correct value");
+}
+
+-(void) testManglingTypes
+{
+#define MANGLE(...) STR(CC_MANGLE_TYPE(__VA_ARGS__))
+    
+    XCTAssertEqualObjects(MANGLE(int), @"I32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(int var), @"I32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(PTYPE(int)), @"pI32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(PTYPE(PTYPE(int))), @"ppI32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(TYPE(int)), @"int", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(FPTYPE(int(*)(int, float))), @"fp2I32_I32_F32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(FPTYPE(FPTYPE(void(*)(void))(*)(int))), @"fp1fp1V_V_I32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(FPTYPE(int(*)(int, FPTYPE(void(*)(void))))), @"fp2I32_I32_fp1V_V", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(FPTYPE(int(*)(int, FPTYPE(FPTYPE(char(*)(PTYPE(char)*))(*)(void))))), @"fp2I32_I32_fp1fp1I8_pI8_V", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(FPTYPE(int(*)(int a, float b))), @"fp2I32_I32_F32", @"should have correct value");
+    XCTAssertEqualObjects(MANGLE(FPTYPE(int(*)(const int a, float b))), @"fp2I32_I32_F32", @"should have correct value");
 }
 
 @end
