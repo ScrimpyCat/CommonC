ad74572467cff315df3b877eb4e76e2fd2467dfb
Stefan Johnson <sj@scrimpycat.io>
2021-01-23 12:49:48 +1100

    Test read swapped endianness

--- CommonCTests/MemoryTests.m
+++ CommonCTests/MemoryTests.m
@@ -60,6 +60,18 @@
     XCTAssertEqual(aOut[8], 1, @"Should have the correct value");
     XCTAssertEqual(aOut[9], 2, @"Should have the correct value");
     
+    CCMemoryReadBig(aIn, sizeof(aIn), 1, sizeof(aOut), aOut);
+    XCTAssertEqual(aOut[0], 2, @"Should have the correct value");
+    XCTAssertEqual(aOut[1], 3, @"Should have the correct value");
+    XCTAssertEqual(aOut[2], 1, @"Should have the correct value");
+    XCTAssertEqual(aOut[3], 2, @"Should have the correct value");
+    XCTAssertEqual(aOut[4], 3, @"Should have the correct value");
+    XCTAssertEqual(aOut[5], 1, @"Should have the correct value");
+    XCTAssertEqual(aOut[6], 2, @"Should have the correct value");
+    XCTAssertEqual(aOut[7], 3, @"Should have the correct value");
+    XCTAssertEqual(aOut[8], 1, @"Should have the correct value");
+    XCTAssertEqual(aOut[9], 2, @"Should have the correct value");
+    
     uint16_t bIn[3] = { 0x01f1, 0x02f2, 0x03f3 }, bOut[10];
     CCMemoryRead(bIn, sizeof(bIn), 0, sizeof(bOut), bOut);
     XCTAssertEqual(bOut[0], 0x01f1, @"Should have the correct value");
@@ -85,6 +97,18 @@
     XCTAssertEqual(bOut[8], 0xf103, @"Should have the correct value");
     XCTAssertEqual(bOut[9], 0xf201, @"Should have the correct value");
     
+    CCMemoryReadBig(bIn, sizeof(bIn), 1, sizeof(bOut), bOut);
+    XCTAssertEqual(bOut[0], 0x01f2, @"Should have the correct value");
+    XCTAssertEqual(bOut[1], 0x02f3, @"Should have the correct value");
+    XCTAssertEqual(bOut[2], 0x03f1, @"Should have the correct value");
+    XCTAssertEqual(bOut[3], 0x01f2, @"Should have the correct value");
+    XCTAssertEqual(bOut[4], 0x02f3, @"Should have the correct value");
+    XCTAssertEqual(bOut[5], 0x03f1, @"Should have the correct value");
+    XCTAssertEqual(bOut[6], 0x01f2, @"Should have the correct value");
+    XCTAssertEqual(bOut[7], 0x02f3, @"Should have the correct value");
+    XCTAssertEqual(bOut[8], 0x03f1, @"Should have the correct value");
+    XCTAssertEqual(bOut[9], 0x01f2, @"Should have the correct value");
+    
     uint32_t cIn[3] = { 0x01f1e1d1, 0x02f2e2d2, 0x03f3e3d3 }, cOut[10];
     CCMemoryRead(cIn, sizeof(cIn), 0, sizeof(cOut), cOut);
     XCTAssertEqual(cOut[0], 0x01f1e1d1, @"Should have the correct value");
@@ -109,6 +133,18 @@
     XCTAssertEqual(cOut[7], 0xd302f2e2, @"Should have the correct value");
     XCTAssertEqual(cOut[8], 0xd103f3e3, @"Should have the correct value");
     XCTAssertEqual(cOut[9], 0xd201f1e1, @"Should have the correct value");
+    
+    CCMemoryReadBig(cIn, sizeof(cIn), 1, sizeof(cOut), cOut);
+    XCTAssertEqual(cOut[0], 0xe1f101d2, @"Should have the correct value");
+    XCTAssertEqual(cOut[1], 0xe2f202d3, @"Should have the correct value");
+    XCTAssertEqual(cOut[2], 0xe3f303d1, @"Should have the correct value");
+    XCTAssertEqual(cOut[3], 0xe1f101d2, @"Should have the correct value");
+    XCTAssertEqual(cOut[4], 0xe2f202d3, @"Should have the correct value");
+    XCTAssertEqual(cOut[5], 0xe3f303d1, @"Should have the correct value");
+    XCTAssertEqual(cOut[6], 0xe1f101d2, @"Should have the correct value");
+    XCTAssertEqual(cOut[7], 0xe2f202d3, @"Should have the correct value");
+    XCTAssertEqual(cOut[8], 0xe3f303d1, @"Should have the correct value");
+    XCTAssertEqual(cOut[9], 0xe1f101d2, @"Should have the correct value");
 }
 
 @end
